17bdef057a7b746f4ed26aa3ed253ef107117eb3
Jorge Besnier <jorgebesnierb@outlook.com>
2022-09-27 20:34:21 -0500

    git fix?

--- src/models/orderDetails.js
+++ src/models/orderDetails.js
@@ -1,5 +1,4 @@
 const { Model, DataTypes } = require('sequelize');
-<<<<<<< HEAD
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
@@ -35,34 +34,3 @@ User.init(
 );
 
 module.exports = User;
-=======
-const sequelize = require('../config/connection');
-
-class orderDetails extends Model {}
-
-orderDetails.init(
-  {
-    orderId: {
-      type: DataTypes.INTEGER,
-    },
-    dishId: {
-      type: DataTypes.INTEGER,
-    },
-    quantity: {
-      type: DataTypes.INTEGER,
-    },
-    references: {
-        model: 'orders',
-        key: 'orderId',
-      }
-    },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'orderDetails',
-  }
-);
-
-module.exports = orderDetails;
->>>>>>> cf603d81b1e1f4a1e107583699d3289071b1513f
--- src/models/orders.js
+++ src/models/orders.js
@@ -1,5 +1,4 @@
 const { Model, DataTypes } = require('sequelize');
-<<<<<<< HEAD
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
@@ -51,51 +50,3 @@ Orders.init(
 );
 
 module.exports = Orders;
-=======
-const sequelize = require('../config/connection');
-
-class orders extends Model {}
-
-orders.init(
-  {
-    orderId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    customerId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    tableId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    isCooked: {
-      type: DataTypes.BOOLEAN
-    },
-    isDelivered: {
-      type: DataTypes.BOOLEAN
-    },
-    orderDate: {
-      type: DataTypes.DATE
-    },
-    totalCost: {
-      type: DataTypes.DECIMAL
-    },
-    references: {
-        model: 'customers',
-        key: 'id',
-      }
-    },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'orders',
-  }
-);
-
-module.exports = orders;
->>>>>>> cf603d81b1e1f4a1e107583699d3289071b1513f
