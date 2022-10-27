e5aa84b62cd88a552aab7778d4b732d7dff4eda4
Bladeo8x <gerardo50_@hotmail.com>
2022-10-26 22:09:50 -0500

    trying on heroku push

--- controllers/menu.js
+++ controllers/menu.js
@@ -1,18 +1,25 @@
-const router = require('express').Router();
-const menu = require('../models/Menu');
+const router = require("express").Router();
+const menu = require("../models/Menu");
 
 // route to get all dishes
-router.get('/menu', async (req, res) => {
+router.get("/menu", async (req, res) => {
   // Get all books from the book table
   menu.findAll().then((menuData) => {
     // res.json(menuData);
-    const plainMenuData = menuData.map((data) => data.get({plain:true}));
-    //console.log(plainMenuData);
-    res.render('menu', {layout: 'menu-layout', plainMenuData});
-
+    const plainMenuData = menuData.map((data) => data.get({ plain: true }));
+    const dishes = plainMenuData.filter((menu) => menu.dishType === "1");
+    const snacks = plainMenuData.filter((menu) => menu.dishType === "2");
+    const desserts = plainMenuData.filter((menu) => menu.dishType === "3");
+    const drinks = plainMenuData.filter((menu) => menu.dishType === "4");
+    // console.log(dishes, plainMenuData);
+    res.render("menu", {
+      layout: "menu-layout",
+      dishes,
+      snacks,
+      desserts,
+      drinks,
+    });
   });
 });
-  
 
 module.exports = router;
-
